8eff2eb8767bdbdc4a56d69b5597ef1005c969c4
tomas-aguado <tomas.aguado@gmail.com>
2017-07-02 08:20:00 +0200

    Fixed legend

--- plot3.R
+++ plot3.R
@@ -12,11 +12,12 @@ dataFrame$Sub_metering_3 <- as.numeric(gsub(',','.',dataFrame$Sub_metering_3))
 #We add and extra column with the timestamp combining both Time and date
 dataFrame <- transform(dataFrame, datetime=as.POSIXct(paste(dataFrame$Date, dataFrame$Time)), "%d/%m/%Y %H:%M:%S")
 #We plot adding the extra data with their label tipe and colour
+#We change the expor procedure because copy messes up the legend
+png(filename = "plot3.png")
 plot(dataFrame$datetime, dataFrame$Sub_metering_1, type="l",xlab="", ylab="Energy sub metering")
 lines(dataFrame$datetime,dataFrame$Sub_metering_2,col="red")
 lines(dataFrame$datetime,dataFrame$Sub_metering_3,col="blue")
 #we add the legend
-size <- legend("topright",col=c("black", "red", "blue"),c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=1)
-#We export to a file of the given size and name
-dev.copy(png, file="plot3.png", width=480, height=480)
+size <- legend("topright",col=c("black", "red", "blue"),c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lwd = 1)
+#We close the device
 dev.off()
--- plot4.R
+++ plot4.R
@@ -14,6 +14,7 @@ dataFrame$Sub_metering_3 <- as.numeric(gsub(',','.',dataFrame$Sub_metering_3))
 #We add and extra column with the timestamp combining both Time and date
 dataFrame <- transform(dataFrame, datetime=as.POSIXct(paste(dataFrame$Date, dataFrame$Time)), "%d/%m/%Y %H:%M:%S")
 #The layout(2x2) is configured
+png(filename = "plot4.png")
 par(mfrow=c(2,2))
 #First plot
 plot(dataFrame$datetime, dataFrame$Global_active_power,type="l",xlab="",ylab="Global Active Power")
@@ -23,9 +24,8 @@ plot(dataFrame$datetime, dataFrame$Voltage,type="l",xlab="datetime",ylab="Voltag
 plot(dataFrame$datetime, dataFrame$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(dataFrame$datetime,dataFrame$Sub_metering_2,col="red")
 lines(dataFrame$datetime,dataFrame$Sub_metering_3,col="blue")
-legend("topright",col=c("black", "red", "blue"),c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=1)
+legend("topright",col=c("black", "red", "blue"),c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lwd = 1)
 #Last Plot
 plot(dataFrame$datetime, dataFrame$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
-#We export to a file of the given size and name 
-dev.copy(png, file="plot4.png", width=480, height=480)
+#We close the device
 dev.off()
